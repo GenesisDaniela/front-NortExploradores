0bdd6ec208ff1183cfeb6667bb636d19c9cd75f1
DanielaSanchezb <danielajulianasbay@ufps.edu.co>
2021-11-04 00:23:13 -0500

    Se agrega form aliados

--- src/app/services/empresa.service.ts
+++ src/app/services/empresa.service.ts
@@ -8,12 +8,15 @@ import * as global from 'global'
 })
 export class EmpresaService {
 
-  uri=`${global.url}/paquete/`;
+  uri=`${global.url}/empresa/`;
   constructor(private http: HttpClient) { }
 
   public listarEmpresa():Observable<any>{
     return this.http.get<any>(this.uri);
   }
 
+  public post(empresa:any):Observable<any>{
+    return this.http.post<any>(this.uri, empresa)
+  }
 
 }
