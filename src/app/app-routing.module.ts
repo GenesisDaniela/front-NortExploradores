669c125017db6671bce85c6f15cc63cc0494e949
Santiago Serrano <santiagoandressp@ufps.edu.co>
2021-11-12 00:21:23 -0500

    Problema de admin quitado, se agrega formulario de registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './clientes/pages/inicio/inicio.component';
 import { FormPagosComponent } from './clientes/pages/pagos/form-pagos/form-pagos.component';
 import { AuthLoginComponent } from './security/auth-login/auth-login.component';
+import { AuthRegisterComponent } from './security/auth-register/auth-register.component';
 import { LoginUsuario } from './security/models/login-usuario';
 import { ErrorPagesComponent } from './shared/error-pages/error-pages.component';
 
 const routes: Routes = [
   {
     path:"login",component:AuthLoginComponent
+  },
+  {
+    path:"registro",component:AuthRegisterComponent
   },
   {
     path:"pago",component:FormPagosComponent
--- src/app/security/auth-register/auth-register.component.ts
+++ src/app/security/auth-register/auth-register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenService } from 'src/app/services/token.service';
+import { NuevoUsuario } from '../models/nuevo-usuario';
 
 @Component({
   selector: 'app-auth-register',
@@ -6,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthRegisterComponent implements OnInit {
 
-  constructor() { }
+  nuevoUsuario?: NuevoUsuario;
+  nombreUsuario="";
+  email=""
+  password=""
+  errMsj?: string;
+  isLogged = false;
+
+
+  constructor(private tokenService: TokenService,
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+  onRegister(): void {
+    this.nuevoUsuario = new NuevoUsuario(this.nombreUsuario, this.email, this.password);
+    this.authService.nuevo(this.nuevoUsuario).subscribe(
+      data => {
+        // this.toastr.success('Cuenta Creada', 'OK', {
+        //   timeOut: 3000, positionClass: 'toast-top-center'
+        // });
+        this.router.navigate(['/login']);
+      },
+      err => {
+        this.errMsj = err.error.mensaje;
+        window.alert(this.errMsj);
+        // this.toastr.error(this.errMsj, 'Fail', {
+        //   timeOut: 3000,  positionClass: 'toast-top-center',
+        // });
+        // console.log(err.error.message);
+      }
+    );
   }
 
 }
