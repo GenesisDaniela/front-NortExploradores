e34288bd22a32c7c811bf57e56070e8a85a59ac7
CamiloRamirezP <camiloandresrper@ufps.edu.co>
2021-11-13 12:23:11 -0500

    Pasadia - Estadia

--- src/app/clientes/pages/tours/pasadia/pasadia.component.ts
+++ src/app/clientes/pages/tours/pasadia/pasadia.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { PaqueteService } from 'src/app/services/paquete.service';
+import { TourService } from 'src/app/services/tour.service';
+
 @Component({
   selector: 'app-pasadia',
   templateUrl: './pasadia.component.html',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasadiaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private paqueteService:PaqueteService,
+    private tourService:TourService
+    ) { }
+
+  paquetes:any =[];
+  tours: any= [];
 
   ngOnInit(): void {
+    this.listarPaquetes();
+    this.listarTour();
   }
+
+
+  public listarTour(){
+    this.tourService.listarTourActivo().subscribe(tour=>{
+      this.tours=tour
+    })
+  }
+
+  public listarPaquetes(){
+    this.paqueteService.listarPaqPasadia().subscribe(listaPaquetes=>{
+      this.paquetes = listaPaquetes;
+    })
+  }
+
 
 }
