298df6cff68707a25db55bfa02f5e6e759b2d385
BrayanRoa <brayanandresrl@ufps.edu.co>
2021-11-13 23:00:40 -0500

    info corporativa del admin terminado

--- src/app/administracion/pages/inf-corporativa/edit-info/edit-info.component.ts
+++ src/app/administracion/pages/inf-corporativa/edit-info/edit-info.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmpresaService } from '../../../../services/empresa.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-info',
@@ -7,9 +10,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditInfoComponent implements OnInit {
 
-  constructor() { }
+  id  :any;
+  form:FormGroup;
+  data:any;
+  estado :any;
+
+  constructor(private aRoute: ActivatedRoute, private empresa: EmpresaService, private fb: FormBuilder, private router : Router) { 
+
+    this.form = this.fb.group({
+      idEmpresa  : ["", Validators.required],
+      nombre      : ["", Validators.required],
+      mision      : ["", Validators.required],
+      vision      : ["", Validators.required],
+      descripcion : ["", Validators.required],
+      direccion   : ["", Validators.required],
+      urlImagen   : ["", Validators.required],
+      correo      : ["", Validators.required],
+      estado      : ["", Validators.required],
+      telefono    : ["", Validators.required],
+      fecha       : ["", Validators.required]
+    })
+  }
 
   ngOnInit(): void {
+    this.id = this.aRoute.snapshot.paramMap.get("idEmp");
+    
+
+    this.empresa.obtenerEmpresa(this.id).subscribe(data=>{
+      console.log(data)
+      this.data =data;
+
+      if(data.estado){
+        this.estado="activo";
+      }else{
+        this.estado="inactivo"
+      }
+
+      this.form.setValue({
+          idEmpresa   : data.idEmpresa,
+          nombre      : data.nombre,
+          mision      : data.mision,
+          vision      : data.vision,
+          descripcion : data.descripcion,
+          direccion   : data.direccion,
+          urlImagen   : data.urlImagen,
+          correo      : data.correo,
+          estado      : data.estado,
+          telefono    : data.telefono,
+          fecha       : data.fecha
+      })
+    })
+    
+  }
+
+  enviarData(){
+    this.empresa.editarEmpresa(this.id, this.form.value).subscribe(data=>{
+       console.log(this.form.value);
+       this.router.navigateByUrl("/administracion/infoCorporativa");
+    })
+    console.log(this.form.value)
   }
 
 }
