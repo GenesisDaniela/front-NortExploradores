c7494e10977b67d3c573eea5f6e874549e07d731
JhonnyGCH <55067247+JhonnyGCH@users.noreply.github.com>
2021-11-14 02:28:26 -0500

    Omitir seguridad en src de iframe

--- src/app/clientes/pages/tours/detalle-tour/detalle-tour.component.ts
+++ src/app/clientes/pages/tours/detalle-tour/detalle-tour.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SecurityContext } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { TourService } from 'src/app/services/tour.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-detalle-tour',
@@ -11,15 +12,19 @@ export class DetalleTourComponent implements OnInit {
 
   id: any|null = "";
   data: any;
+  url: any;
 
   constructor(
+    private domSanitizer:DomSanitizer,
     private aRoutes: ActivatedRoute, private tour: TourService ) {   this.id = this.aRoutes.snapshot.paramMap.get("idTour");
   }
   
 
   ngOnInit(): void {
     this.tour.encontrarTour(this.id).subscribe(data => {
-      this.data=data;      
+      this.data=data;
+      this.url = data.paquete.municipio.urlUbicacion;
+      this.url = this.domSanitizer.bypassSecurityTrustResourceUrl( this.url)
     })
   }
 
