9b152df3fbe6d74bbe180b77176a999f400075c2
BrayanRoa <brayanandresrl@ufps.edu.co>
2021-11-27 14:23:16 -0500

    formulario de paquetes y alojamientos

--- src/app/administracion/administracion-routing.module.ts
+++ src/app/administracion/administracion-routing.module.ts
@@ -93,7 +93,7 @@ const routes : Routes = [
       {path:"dashboard", component:DashboardComponent},
 
       {path:"calendario", component:CalendarioComponent},
-      {path:"**", redirectTo:"reportes"}
+      {path:"**", redirectTo:"dashboard"}
   ]}
 ]
 
