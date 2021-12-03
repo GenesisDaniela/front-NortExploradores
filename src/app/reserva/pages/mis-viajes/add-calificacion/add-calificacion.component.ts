4d1a080d0c8a0c2af5b5aa456f270802134e566f
FelipeM09 <manuelfelipeme@ufps.edu.co>
2021-12-03 16:03:57 -0500

    El formulario de calificaciones es funcional

--- src/app/reserva/pages/mis-viajes/add-calificacion/add-calificacion.component.ts
+++ src/app/reserva/pages/mis-viajes/add-calificacion/add-calificacion.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../../../../services/usuario.service';
 import { CompraService } from '../../../../services/compra.service';
 import { TokenService } from '../../../../services/token.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CalificacionService } from '../../../../services/calificacion.service';
 import { ToastrService } from 'ngx-toastr';
@@ -20,6 +20,11 @@ export class AddCalificacionComponent implements OnInit {
   public compras: any;  
   public comprasUsuario: any[]=[];  
   public form!: FormGroup;
+  public idCompra:string|null;
+  public idTour!: number;
+  public idCompra2!:number;
+  public nombrePaq!:string;
+  
 
   constructor(
     private usuarioSer: UsuarioService,
@@ -28,10 +33,17 @@ export class AddCalificacionComponent implements OnInit {
     private router: Router,
     private calificacion: CalificacionService,
     private formBuilder: FormBuilder,
+    private aRouter: ActivatedRoute,
     private toastr: ToastrService 
-  ) { }
+
+  ) { this.idCompra = aRouter.snapshot.paramMap.get('idCompra');
+    }
 
   ngOnInit(): void {
+
+    this.cargarToken();  
+    this.nombreUser=this.tokenS.getUserName(); 
+    this.cargarUsuario();
 
     this.form = this.formBuilder.group({   
       puntuacion: ['',
@@ -45,8 +57,15 @@ export class AddCalificacionComponent implements OnInit {
           Validators.required,
           Validators.minLength(10),
           Validators.maxLength(255)
-        ])]      
+        ])],
+        usuario: ['',
+      Validators.required],
+      tour: ['',
+      Validators.required]       
     });
+
+   
+
   }
 
   public cargarToken() {
@@ -68,27 +87,53 @@ export class AddCalificacionComponent implements OnInit {
   public cargarPaquetesComprados(id: number) {
     this.comprasSer.compras().subscribe((compras: any) => {
       this.compras = compras;
-      
+           
+
       for(const iterator of compras){               
         if(id===iterator.usuario.id_Usuario){          
           this.comprasUsuario.push(iterator)
+          
         }  
       }
       
-
+      this.compararCompra(this.idCompra,this.comprasUsuario)
+      
     }) 
-  }    
+  } 
+  
+    public compararCompra(id: string | null, comprasUsuario: any[]) {
+      for( let iterator of comprasUsuario){  
+       
+                 
+        if(id==iterator.idCompra){          
+         this.idTour=iterator.tour.idTour
+         this.nombrePaq=iterator.tour.paquete.nombre
+         
+         this.nombrePaquete(this.nombrePaq)
+        }  
+      }
+      
+    } 
 
   public enviarData(){
     
-   /*  this.form.controls.usuario.setValue(this.idUsuario) */
+    
+    this.form.controls.usuario.setValue(this.idUsuario)
+    this.form.controls.tour.setValue(this.idTour)
+    
     
     this.calificacion.post(this.form.value).subscribe(data=>{
       this.toastr.success("Calificación Agregada!", "Registrada", {
         positionClass: 'toast-top-center'
        }) 
-      this.router.navigateByUrl("/calificar")
+      this.router.navigateByUrl("/misViajes")
     })
+  }
+
+  public nombrePaquete(nombre:any){
+    const nombrePaq = document.getElementById("nomPaquete")
+    if(nombrePaq) nombrePaq.innerHTML=nombre
+    
   }
 
 }
