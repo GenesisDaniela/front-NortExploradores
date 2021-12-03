1bf4596709815c7085e6ef20957e1ddc392c006f
FelipeM09 <manuelfelipeme@ufps.edu.co>
2021-12-03 16:44:39 -0500

    Se actualizó contactos con la info de la empresa real

--- src/app/reserva/pages/contacto/contacto.component.ts
+++ src/app/reserva/pages/contacto/contacto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EmpresaService } from '../../../services/empresa.service';
 
 @Component({
   selector: 'app-contacto',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
+  data: any[] = [];
+  tel: any;
+  email:any;
 
-  constructor() { }
+  constructor(private httpClient: EmpresaService) { }
 
   ngOnInit(): void {
+    this.httpClient.obtenerEmpresa("84950").subscribe((data:any)=>{
+      this.data = data; 
+      this.tel = data.telefono;
+      this.email = data.correo;     
+    })
   }
 
 }
